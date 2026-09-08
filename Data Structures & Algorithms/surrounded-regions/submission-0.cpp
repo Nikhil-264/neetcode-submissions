9118e616e88bf05ec3ecbb7af3a2aa6e93af2c19
Nikhil Mahindrakar <137866121+Nikhil-264@users.noreply.github.com>
2026-09-09 02:18:55 +0530

    Update: surrounded-regions - submission-0

--- Data Structures & Algorithms/surrounded-regions/submission-0.cpp
+++ Data Structures & Algorithms/surrounded-regions/submission-0.cpp
@@ -1,99 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int n = heights.size();
-        int m = heights[0].size();
-        vector<vector<string>> str(n, vector<string> (m, ""));
+    void dfs(int row, int col, vector<vector<char>> &grid, vector<int> &dir, vector<vector<int>> &vis){
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+        for(int i = 0; i < 4; i++){
+            int nRow = row + dir[i];
+            int nCol = col + dir[i + 1];
+
+            if(nRow >= 0 and nRow < n and nCol >= 0 and nCol < m and !vis[nRow][nCol] and grid[nRow][nCol] == 'O'){
+                dfs(nRow, nCol, grid, dir, vis);
+            }
+        }
+    }
+    void solve(vector<vector<char>>& grid) {
+        int n = grid.size(); 
+        int m = grid[0].size();
+
+        vector<int> dir = {-1, 0, 1, 0, -1};
 
         vector<vector<int>> vis(n, vector<int> (m, 0));
-        // for altantic
-        queue<pair<int, pair<int, int>>> q;
-        //value, row, col
 
         for(int i = 0; i < n; i++){
-            if(vis[i][m - 1] == 0){
-                q.push({heights[i][m - 1], {i, m - 1}});
-                vis[i][m - 1] = 1;
+            if(!vis[i][0] and grid[i][0] == 'O'){
+                dfs(i, 0, grid, dir, vis);
             }
-        }
-        for(int j = 0; j < m; j++){
-            if(vis[n - 1][j] == 0){
-                q.push({heights[n - 1][j], {n - 1, j}});
-                vis[n - 1][j] = 1;
+            if(!vis[i][m - 1] and grid[i][m - 1] == 'O'){
+                dfs(i, m - 1, grid, dir, vis);
             }
         }
 
-        vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while(!q.empty()){
-            int value = q.front().first;
-            int row = q.front().second.first;
-            int col = q.front().second.second;
-            q.pop();
-            str[row][col] += "a";
-
-            for(auto it : dir){
-                int nrow = row + it.first;
-                int ncol = col + it.second;
-
-                if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m){
-                    if(heights[nrow][ncol] >= value and vis[nrow][ncol] == 0){
-                        vis[nrow][ncol] = 1;
-                        q.push({heights[nrow][ncol], {nrow, ncol}});
-                    }
-                }
+        for(int j = 1; j < m - 1; j++){
+            if(!vis[0][j] and grid[0][j] == 'O'){
+                dfs(0, j, grid, dir, vis);
+            }
+            if(!vis[n - 1][j] and grid[n - 1][j] == 'O'){
+                dfs(n - 1, j, grid, dir, vis);
             }
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                vis[i][j] = 0;
-            }
-        }
-
-        for(int i = 0; i < n; i++){
-            if(vis[i][0] == 0){
-                q.push({heights[i][0], {i, 0}});
-                vis[i][0] = 1;
-            }
-        }
-        for(int j = 0; j < m; j++){
-            if(vis[0][j] == 0){
-                q.push({heights[0][j], {0, j}});
-                vis[0][j] = 1;
-            }
-        }
-
-        while(!q.empty()){
-            int value = q.front().first;
-            int row = q.front().second.first;
-            int col = q.front().second.second;
-            q.pop();
-            
-            str[row][col] += "p";
-
-            for(auto it : dir){
-                int nrow = row + it.first;
-                int ncol = col + it.second;
-
-                if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m){
-                    if(heights[nrow][ncol] >= value and vis[nrow][ncol] == 0){
-                        vis[nrow][ncol] = 1;
-                        q.push({heights[nrow][ncol], {nrow, ncol}});
-                    }
+                if(!vis[i][j]){
+                    grid[i][j] = 'X';
                 }
             }
         }
-
-        vector<vector<int>> ans;
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(str[i][j].size() == 2){
-                    ans.push_back({i, j});
-                }
-            }
-        }
-
-        return ans;
     }
 };
